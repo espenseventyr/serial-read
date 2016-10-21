3687bad6d8fd888efbfa6153c555a9b52de16b53
Espen Trydal <espen@fastmail.com>
2016-10-21 14:54:46 +0200

    Cosmetic changes

--- serial-reader.go
+++ serial-reader.go
@@ -50,8 +50,6 @@ func main() {
 
 		//Adding timestamp
 		now := time.Now()
-		fmt.Printf("%v:%v.%v   ", now.Hour(), now.Minute(), now.Second())
-		fmt.Printf("%v ºC\n", temp)
 
 		//Preparing the message and encoding it to JSON
 		m_in := Message{temp, int64(now.Minute()), int64(now.Second())}
@@ -70,7 +68,8 @@ func main() {
 		}
 
 		//Test output
-		fmt.Println(m_in)
-		fmt.Println(m_out)
+		fmt.Printf("%v:%v.%v   ", now.Hour(), now.Minute(), now.Second())
+		fmt.Printf("%v ºC\n", temp)
+		fmt.Printf("%v\n%v\n", m_in, m_out)
 	}
 }
